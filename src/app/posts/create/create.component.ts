4b266ba6183d25c87321c9d1ae474a69f496363e
黃升煌 Mike <mike@miniasp.com>
2020-08-20 17:24:27 +0800

    任務 06：Reactive Forms 繫結表單資料 #7

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray } from '@angular/forms';
+import { timestamp } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
+  tags = this.formBuilder.array([
+    this.formBuilder.control('HTML'),
+    this.formBuilder.control('CSS'),
+    this.formBuilder.control('JavaScript'),
+  ]);
 
-  constructor() { }
+  post = this.formBuilder.group({
+    title: this.formBuilder.control(''),
+    description: this.formBuilder.control(''),
+    body: this.formBuilder.control(''),
+    tags: this.tags,
+  });
 
-  ngOnInit(): void {
+  // get tags(): FormArray {
+  //   return this.post.get('tags') as FormArray;
+  // }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  addTag(tag: string): void {
+    this.tags.push(this.formBuilder.control(tag));
   }
 
+  removeTag(index: number): void {
+    this.tags.removeAt(index);
+  }
 }
