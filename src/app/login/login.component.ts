d8e9c2e7570a1e7610b9c2afbbd51ca46cba55ff
黃升煌 Mike <mike@miniasp.com>
2020-08-20 16:09:50 +0800

    任務 04：在程式中動態切換路由 #5

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,52 @@
+import { LoginService } from './../login.service';
+import { UserLoginInfo } from './../interfaces/login-info';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user: UserLoginInfo = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    // this.loginService.login(this.user).subscribe({
+    //   error: (err: HttpErrorResponse ) => {
+    //     alert(err.message);
+    //   },
+    //   next: (result) => {
+    //     console.log(result.user.token);
+    //     this.router.navigateByUrl('/');
+    //   },
+    //   complete: () => {
+    //     console.log('complete');
+    //   }
+    // });
+
+    this.loginService
+      .login(this.user)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          alert(err.message);
+          return throwError(err);
+        })
+      )
+      .subscribe((result) => {
+        console.log(result.user.token);
+        localStorage.setItem('token', result.user.token);
+        this.router.navigateByUrl('/');
+      });
   }
-
 }
